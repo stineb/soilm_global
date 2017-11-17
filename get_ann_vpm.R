f2fbe4d5b4318cb3132a44c40fc468dbe061f6fb
stineb <benjamin.stocker@gmail.com>
2017-11-18 00:13:05 +0100

    added file for creating VPM

---
+++ get_ann_vpm.R
@@ -0,0 +1,37 @@
+source("~/.Rprofile")
+library(raster)
+library(dplyr)
+library(abind)
+
+yearstart = 2000
+yearend   = 2016
+
+for (year in yearstart:yearend){
+
+  rasta <- raster( paste0("/alphadata01/bstocker/data/gpp_vpm/GPP.VPM.", as.character(year), ".v20.HD.tif") )
+  arr <-  rasta %>% 
+            as.array() %>% aperm( perm = c(2,1,3) )
+  arr <- arr[,,1]
+  
+  if (year==2000){
+    gpp <- arr
+    lon <- seq(-179.75, 179.75, by=0.5)
+    lat <- seq(-89.75,89.75, by=0.5)
+  } else {
+    gpp <- abind( gpp, arr, along=3 )
+  }
+
+}
+
+## write annual GPP to file
+outfilnam <- paste0( myhome, "/data/gpp_vpm/gpp_vpm_ann.nc")
+cdf.write( gpp, "gpp", 
+           lon, rev(lat),
+           filnam = outfilnam,
+           nvars = 1,
+           time = yearstart:yearend,
+           make.tdim = TRUE,
+           units_time = "year",
+           long_name_var1 = "Gross primary productivity",
+           units_var1 = "gC m-2 year-1"
+           )
