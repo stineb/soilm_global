0a64bf55a0fc96458a4f2cc1db9aa956ec7167c6
stineb <b.stocker@imperial.ac.uk>
2018-05-31 16:19:18 +0200

    complemented SI with Fig. S20 (absolute variance).

--- map_effects_gpp_var.R
+++ map_effects_gpp_var.R
@@ -7,7 +7,7 @@ source("~/.Rprofile")
 ## GPP interannual (absolute) variance change
 ##------------------------------------------------------------------------
 fil_s0 <- "gpp_pmodel_s0_VAR.nc"
-fil_s1 <- "gpp_pmodel_s1_VAR.nc"
+fil_s1 <- "gpp_pmodel_s1b_VAR.nc"
 
 dir <- paste0( myhome, "/data/pmodel_fortran_output/v2/")
 
@@ -24,22 +24,25 @@ nc <- nc_open( paste0( dir, fil_s1 ) )
 gpp_s1 <- ncvar_get( nc, varid="gpp" )
 nc_close(nc)
 
-##-----------------------------------------------------
-## Plot absolute variance in S1
-##-----------------------------------------------------
-plot_map( gpp_s1*1e-3, lev=c( 0, 40, 10 ), 
-      toplefttext=expression(paste("GPP aboslute variance")), 
-      toprighttext=expression(paste("gC m"^{-2}, "yr"^{-1})),
-      maxval = 200, color=cols
-      , file = "fig/map_gpp_var_s1.pdf" 
-      )
+# ##-----------------------------------------------------
+# ## Plot absolute variance in S1
+# ##-----------------------------------------------------
+# plot_map( gpp_s1*1e-3, lev=c( 0, 40, 10 ), maxval = 200 )
+# 
+# ##-----------------------------------------------------
+# ## Difference in absolute variance
+# ##-----------------------------------------------------
+# plot_map( (gpp_s1 - gpp_s0)*1e-3, lev=c( -10, 10, 10 ), positive = FALSE, maxval=60, minval=-60 )
 
 ##-----------------------------------------------------
-## Difference in absolute variance
+## Amplification in absolute variance
 ##-----------------------------------------------------
-plot_map( (gpp_s1 - gpp_s0)*1e-3, lev=c( -10, 10, 10 ), 
-          toplefttext=expression(paste("GPP change in absolute variance")), 
-          toprighttext=expression(paste("kgC m"^{-2}, "yr"^{-1})),
-          positive = FALSE, maxval=60, minval=-60
-          , file = "fig/map_gpp_var_diff.pdf"
-          )
+color <- c( "royalblue4", "wheat", "tomato2", "tomato4" )
+lev <- c( 0, 4, 10 )
+maxval = 35
+minval = NA
+# par( mar=c(4,3,3,1),xaxs="i", yaxs="i",las=1, mgp=c(3,1,0))
+out.mycolorbar <- mycolorbar( color, lev, orient="v", plot=FALSE, maxval=maxval, minval=minval )
+
+plot_map( gpp_s1/gpp_s0, lev=lev, maxval=maxval, color = out.mycolorbar$colors )
+
